b6cdef397f729912396b9cd862d7740118f7fb81
Miguel Botón <mboton@lowlevel.st>
2021-06-25 11:25:22 +0200

    Use proper layout for input dialog depending if hint has been provided or not

--- input/src/main/java/com/afollestad/materialdialogs/input/DialogInputExt.kt
+++ input/src/main/java/com/afollestad/materialdialogs/input/DialogInputExt.kt
@@ -95,7 +95,13 @@ fun MaterialDialog.input(
   allowEmpty: Boolean = false,
   callback: InputCallback = null
 ): MaterialDialog {
-  customView(R.layout.md_dialog_stub_input)
+  val layoutId = if (hint != null || hintRes != null) {
+    R.layout.md_dialog_stub_input_layout
+  } else {
+    R.layout.md_dialog_stub_input
+  }
+
+  customView(layoutId)
   onPreShow { showKeyboardIfApplicable() }
   if (!hasActionButtons()) {
     positiveButton(android.R.string.ok)
